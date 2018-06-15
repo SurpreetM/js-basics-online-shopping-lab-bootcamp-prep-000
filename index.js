9388333d06a1fd5ac95ded87acc720b15f86770a
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-06-15 14:33:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,14 +25,22 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  if (getCart().length === 0) {return `Your shopping cart is empty.`}
+  if (getCart().length === 0) {return `Your shopping cart is empty.`} else {
+    cartDescription()
+  }
 }
 
 function cartDescription() {
   var openingSentence = `In your cart, you have`
   
   if (getCart().length >= 1) {
-    openingSentence =+
+    openingSentence += `${getCart()[0].itemName} at ${getCart()[0]}.itemPrice`
+    
+    if (getCart().length >=2) {
+      for (let i=1, l=getCart().length-1; i<l; i++) {
+        openingSentence += `${getCart()[i].itemName} at ${getCart()[i]}.itemPrice`
+      }
+    }
     
   }
   
