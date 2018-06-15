1391b23e2a9aa12a8b55f84d7605bab047c44d40
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-06-15 14:37:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,14 +27,14 @@ function addToCart(item) {
 function viewCart() {
   if (getCart().length === 0) {return `Your shopping cart is empty.`} else {
     
-    var openingSentence = `In your cart, you have`
+    var openingSentence = `In your cart, you have `
   
   if (getCart().length >= 1) {
-    openingSentence += `${getCart()[0].itemName} at ${getCart()[0]}.itemPrice`
+    openingSentence += `${getCart()[0].itemName} at ${getCart()[0].itemPrice}`
     
     if (getCart().length >=2) {
       for (let i=1, l=getCart().length-1; i<l; i++) {
-        openingSentence += `, and ${getCart()[i].itemName} at ${getCart()[i]}.itemPrice`
+        openingSentence += `, and ${getCart()[i].itemName} at ${getCart()[i].itemPrice}`
       }
     }
     
