71e2f319fcac51e97a1e914bc225258f9b87cb5e
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-06-15 14:53:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function viewCart() {
 function total() {
   var total = 0
   for (let i = 0, l=getCart().length; i<l; i++) {
-    total = total + getCart().itemPrice[i]
+    total = parseInt(total) + parseInt(getCart().itemPrice[i])
   }
   return total
 }
